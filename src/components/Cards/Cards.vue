72d03bc46b3f244e5b105a2d2955b63ba8f86c15
brulikmas <lalkamasbeats@gmail.com>
2023-04-02 19:47:38 +0300

    dz3 done

--- src/components/Cards/Cards.vue
+++ src/components/Cards/Cards.vue
@@ -1,6 +1,7 @@
 <script setup>
   import { ref }  from 'vue';
   import css from '../../App.module.css';
+  import OneCard from './OneCard.vue';
 
   const textForFind = ref('');
   const cards = [
@@ -22,15 +23,19 @@
     }
   ]
 
+  function findCard(card) {
+    return card.name.toLowerCase().includes(textForFind.value.toLowerCase());
+  }
+
 </script>
 
 <template>
   <input :class="css.input" v-model="textForFind">
   <template v-for="(item, index) in cards" :key="index">
-    <div :class="css.card" v-if="item.name.includes(textForFind)">
+    <OneCard v-if="findCard(item)" v-slot="{ discountForUser }">
       <h2>{{ item.name }}</h2>
-      <p>{{ item.price }}</p>
-    </div>
+      <p>{{ item.price }} Your discount: {{ discountForUser }}</p>
+    </OneCard>
   </template>
 
 </template>
